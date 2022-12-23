c4e3a5ba77f7eb23648db3d99f845f3323142052
MonicaAuditore <mauditore@live.it>
2022-12-23 13:53:42 +0100

    fix_markup statico

--- js/script.js
+++ js/script.js
@@ -14,3 +14,6 @@ Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella mile
 
 MILESTONE 3
 Al click dell'utente sulle frecce, il programma cambierà l'immagine attiva, che quindi verrà visualizzata al posto della precedente.*/
+
+
+const 
