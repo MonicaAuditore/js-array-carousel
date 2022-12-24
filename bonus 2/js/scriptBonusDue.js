eaab28b459d15062651013081cde6fcc7abfc2a6
MonicaAuditore <mauditore@live.it>
2022-12-24 18:32:02 +0100

    add_collegamento slides D/S

--- bonus 2/js/scriptBonusDue.js
+++ bonus 2/js/scriptBonusDue.js
@@ -11,8 +11,19 @@ const images = [
   "img/05.webp",
 ];
 
+const imagesSin = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp",
+];
+
 const carouselElement = document.querySelector(".caroselloDestra");
 console.log(carouselElement);
+
+const carouselElementSin = document.querySelector(".caroselloSinistra");
+console.log(carouselElementSin);
 
 for (let index = 0; index < images.length; index++) {
   console.log(images[index]);
@@ -20,19 +31,23 @@ for (let index = 0; index < images.length; index++) {
   carouselElement.innerHTML += `<div class="slide">
                                         <img src="${images[index]}">
                                     </div>`;
+
+  carouselElementSin.innerHTML += `<div class="slideSin">
+                                        <img src="${imagesSin[index]}">
+                                    </div>`;
 }
 
 const allSlides = document.querySelectorAll(".slide");
 console.log("allSlides", allSlides);
 
-let currentSlide = 0; // Prima slide attiva
+const allSlidesSin = document.querySelectorAll(".slideSin");
+console.log("allSlidesSin", allSlidesSin);
 
-// allSlides[0].classList.add("uno");
-// allSlides[2].classList.add("tre");
-// allSlides[3].classList.add("quattro");
-// allSlides[4].classList.add("cinque");
+let currentSlide = 0; // Prima slide attiva
+let currentSlideSin = 0; // Prima slide attiva
 
 allSlides[0].classList.add("trasparente");
+allSlidesSin[0].classList.add("current");
 
 const previousArrow = document.querySelector(".previous");
 const nextArrow = document.querySelector(".next");
@@ -43,15 +58,23 @@ nextArrow.addEventListener(
   function () {
     if (currentSlide < allSlides.length - 1) {
       console.log("ho cliccato su .next");
+
       allSlides[currentSlide].classList.remove("trasparente");
+      allSlidesSin[currentSlideSin].classList.remove("current");
+
       currentSlide = currentSlide + 1;
+      currentSlideSin = currentSlideSin + 1;
 
       allSlides[currentSlide].classList.add("trasparente");
+      allSlidesSin[currentSlideSin].classList.add("current");
     } else if (currentSlide == allSlides.length - 1) {
       console.log("torno indietro");
       allSlides[currentSlide].classList.remove("trasparente");
+      allSlidesSin[currentSlideSin].classList.remove("current");
       currentSlide = 0;
+      currentSlideSin = 0;
       allSlides[currentSlide].classList.add("trasparente");
+      allSlidesSin[currentSlideSin].classList.add("current");
     }
   }
 );
@@ -63,15 +86,20 @@ previousArrow.addEventListener(
     if (currentSlide <= allSlides.length - 1 && currentSlide > 0) {
       console.log("ho cliccato su .previous");
       allSlides[currentSlide].classList.remove("trasparente");
+      allSlidesSin[currentSlideSin].classList.remove("current");
       currentSlide = currentSlide - 1;
-
+      currentSlideSin = currentSlideSin - 1;
       allSlides[currentSlide].classList.add("trasparente");
+      allSlidesSin[currentSlideSin].classList.add("current");
     } else if (currentSlide == 0) {
       console.log("torno indietro");
 
       allSlides[currentSlide].classList.remove("trasparente");
+      allSlidesSin[currentSlideSin].classList.remove("current");
       currentSlide = 4;
+      currentSlideSin = 4;
       allSlides[currentSlide].classList.add("trasparente");
+      allSlidesSin[currentSlideSin].classList.add("current");
     }
   }
 );
